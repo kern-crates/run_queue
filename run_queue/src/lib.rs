52badbee6fb65d8b14721fbbd97ecec97957563d
Shi Lei <shi_lei@massclouds.com>
2024-05-18 08:20:57 +0000

    init

--- run_queue/src/lib.rs
+++ run_queue/src/lib.rs
@@ -2,9 +2,7 @@
 
 use taskctx::CtxRef;
 use crate::run_queue::RUN_QUEUE;
-use spinirq::SpinNoIrq;
-
-mod bootcell;
+use spinbase::SpinNoIrq;
 
 #[macro_use]
 extern crate log;
@@ -13,20 +11,20 @@ mod run_queue;
 pub use run_queue::AxRunQueue;
 
 pub fn init() {
-    RUN_QUEUE.init(AxRunQueue::new());
+    RUN_QUEUE.init_by(AxRunQueue::new());
 }
 
 pub fn task_rq(_task: &CtxRef) -> &SpinNoIrq<AxRunQueue> {
-    RUN_QUEUE.get()
+    &RUN_QUEUE
 }
 
 pub fn force_unlock() {
-    unsafe { RUN_QUEUE.get().force_unlock() }
+    unsafe { RUN_QUEUE.force_unlock() }
 }
 
 /// Handles periodic timer ticks for the task manager.
 ///
 /// For example, advance scheduler states, checks timed events, etc.
 pub fn on_timer_tick() {
-    RUN_QUEUE.get().lock().scheduler_timer_tick();
+    RUN_QUEUE.lock().scheduler_timer_tick();
 }
